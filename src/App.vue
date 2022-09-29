831fa7dcf7d2309a8fffde25188b19aa64863879
keeperlee <leejamesjian@163.com>
2022-09-30 00:06:56 +0800

    Suspense的拓展用法及onErrorCaptured获取异常信息的用法

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,74 @@
 <template>
     <h1 v-if="loading">Loading!.....</h1>
-<!--    <img v-if="loaded" :src="result.message" >-->
+    <!--    <img v-if="loaded" :src="result.message" >-->
     <Suspense>
+        <!--slot中只能有一个根元素，如果根元素内有多个异步请求的组件，会按顺序依次加载-->
         <template #default>
-            <AsyncShow></AsyncShow>
+            <div>
+                <!--                <AsyncShow></AsyncShow>-->
+                <DogShow></DogShow>
+            </div>
         </template>
         <template #fallback>
             <h1>Suspense loading</h1>
         </template>
     </Suspense>
     <button @click="openModal">Open Modal</button>
-    <Modal :is-open="modalIsOpen" @close-modal = 'onModalClose'>MY MODAL!!!</Modal>
-    <img v-if="loaded" :src="result[0].url" >
+    <Modal :is-open="modalIsOpen" @close-modal='onModalClose'>MY MODAL!!!</Modal>
+    <img v-if="loaded" :src="result[0].url" alt="">
+    <p>{{errorCaptured}}</p>
 </template>
 
 <script lang="ts">
-import { defineComponent,watch,ref} from 'vue';
+import {defineComponent, watch, ref ,onErrorCaptured} from 'vue';
 import useURLLoader from "@/hooks/useURLLoader";
 import Modal from "@/components/Modal.vue";
 import AsyncShow from '@/components/AsyncShow.vue'
+import DogShow from "@/components/DogShow.vue";
+
 interface DogResult {
-    message:string,
-    status:string
+    message: string,
+    status: string
 }
 
-interface CatResult{
-    id:string,
-    url:string,
-    width:number,
-    height:number
+interface CatResult {
+    id: string,
+    url: string,
+    width: number,
+    height: number
 }
+
 export default defineComponent({
     name: 'App',
-    components: {Modal,AsyncShow},
+    components: {
+        Modal,
+        // AsyncShow,
+        DogShow
+    },
     setup() {
+        const errorCaptured = ref(null)
+        onErrorCaptured((e:any)=>{
+            errorCaptured.value = e
+            return true
+        })
         // const {result,loading,loaded,error} = useURLLoader<DogResult>('https://dog.ceo/api/breeds/image/random')
-        const {result,loading,loaded,error} = useURLLoader<CatResult[]>('https://api.thecatapi.com/v1/images/search')
+        const {result, loading, loaded, error} = useURLLoader<CatResult[]>('https://api.thecatapi.com/v1/images/search')
 
-        watch(result,()=>{
-            if(result.value){
-                console.log('value',result.value[0])
+        watch(result, () => {
+            if (result.value) {
+                console.log('value', result.value[0])
             }
         })
 
-
         const modalIsOpen = ref(false)
-        const openModal = ()=>{
+        const openModal = () => {
             modalIsOpen.value = true
         }
-        const onModalClose = ()=>{
+        const onModalClose = () => {
             modalIsOpen.value = false
         }
         return {
-            result,loading,loaded,error,modalIsOpen,openModal,onModalClose
+            result, loading, loaded, error, modalIsOpen, openModal, onModalClose,errorCaptured
         }
     }
 });
---
+++ src/components/DogShow.vue
@@ -0,0 +1,23 @@
+<template>
+    <img :src="result && result.message" alt="">
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import axios from "axios";
+
+export default defineComponent({
+    name: "DogShow",
+    async setup() {
+        const rawData = await axios.get('https://dog.ceo/api/breeds/image/random')
+        // const rawData = await axios.get('https://dog.ceo/api/breeds/image/ran1dom')
+        return {
+            result:rawData.data
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>

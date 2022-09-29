fbd044a3930da67e8a6be12d59005588d91d5609
keeperlee <leejamesjian@163.com>
2022-09-30 00:36:25 +0800

    setup语法糖

---
+++ src/App2.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="app-2">
+        <h1>{{ hello }}</h1>
+        <h1>{{ count }}</h1>
+        <button @click="count++">添加</button>
+        <hello-world msg="6666666666"></hello-world>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+const hello = 'hello world'
+const count = ref(0)
+</script>
+
+<style scoped>
+
+</style>

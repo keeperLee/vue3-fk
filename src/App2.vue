b7e9a42921a4d4ac1ea637756b80bcf842b21a9b
keeperlee <leejamesjian@163.com>
2022-09-30 00:50:00 +0800

    使用defineProps和defineEmits

--- src/App2.vue
+++ src/App2.vue
@@ -3,15 +3,20 @@
         <h1>{{ hello }}</h1>
         <h1>{{ count }}</h1>
         <button @click="count++">添加</button>
-        <hello-world msg="6666666666"></hello-world>
+        <Message text="牛逼" @change="showAlert"></Message>
+        <hello-world msg="6666666666" ></hello-world>
     </div>
 </template>
 
 <script setup lang="ts">
 import {ref} from "vue";
 import HelloWorld from "@/components/HelloWorld.vue";
+import Message from "@/components/Message.vue"
 const hello = 'hello world'
 const count = ref(0)
+const showAlert = (text:string) =>{
+    alert(text)
+}
 </script>
 
 <style scoped>
---
+++ src/components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="message">
+        <h1>{{text}}</h1>
+        <button @click="onChange">发送自定义事件</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {defineProps ,defineEmits} from "vue";
+const props = defineProps({text:String})
+
+const emit = defineEmits(['change','focus'])
+const onChange =  ()=>{
+    emit('change',props.text)
+}
+</script>
+
+<style scoped>
+
+</style>
